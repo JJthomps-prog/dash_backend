374f5d09c713d77a4ce1b5210a384785978daade
钦越涵 <qinyiqinyuehan@qq.com>
2024-06-23 09:29:08 +0800

    fix bug

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const routes = require('./routes/index'); 
 const bodyParser = require('body-parser');
+const cors = require('cors')
 
 const app = express();
 const port = process.env.PORT || 3000;
